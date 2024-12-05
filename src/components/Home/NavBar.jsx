64266678aaa455424341ec8742cab3d6efaca16a
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-12-05 19:35:00 +0600

    auth provider, navbar, footer almost complete

---
+++ src/components/Home/NavBar.jsx
@@ -0,0 +1,76 @@
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../providers/AuthProvider";
+import UserIcon from "../common/UserIcon";
+
+const NavBar = () => {
+  const { user } = useContext(AuthContext);
+
+  const items = [
+    <li key="home" className="text-black font-roboto font-medium">
+      <Link to="/">Home</Link>
+    </li>,
+    <li key="allvisas" className="text-black font-roboto font-medium">
+      <Link to="/allvisas">All Visas</Link>
+    </li>,
+    <li key="addVisa" className="text-black font-roboto font-medium">
+      <Link to="/addVisa">Add Visa</Link>
+    </li>,
+    <li key="myAddedVisa" className="text-black font-roboto font-medium">
+      <Link to="/myAddedVisa">My Added Visa</Link>
+    </li>,
+    <li key="myVisaApplication" className="text-black font-roboto font-medium">
+      <Link to="/myVisaApplication">My Visa Application</Link>
+    </li>,
+    <li key="profilePage" className="text-black font-roboto font-medium">
+      <Link to="/profilePage">Profile</Link>
+    </li>,
+  ];
+  return (
+    <div>
+      <div className="navbar bg-base-100 shadow p-3">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[999] mt-3 w-52 p-2 shadow">
+              {items}
+            </ul>
+          </div>
+          <a className="btn btn-ghost text-xl">VisaEase</a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{items}</ul>
+        </div>
+        <div className="navbar-end">
+          {user ? (
+            <Link to="/profilePage">
+              <UserIcon user={user} />
+            </Link>
+          ) : (
+            <Link to="/authPage" className="btn">
+              Login
+            </Link>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
---
+++ src/routes/router.jsx
@@ -0,0 +1,76 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../pages/MainLayout";
+import MainLayout from "../pages/MainLayout";
+import Home from "../pages/Home";
+import LoginPage from "../pages/LoginPage";
+import Profile from "../components/Auth/Profile";
+import PageNotFound from "../components/utils/PageNotFound";
+import MyProfile from "../pages/MyProfile";
+import PrivateProvider from "../providers/PrivateProvider";
+import AddVisa from "../pages/AddVisa";
+import MyAddedVisas from "../pages/MyAddedVisas.jsx";
+import MyVisaApplications from "../pages/MyVisaApplications.jsx";
+import AllVisas from "../pages/AllVisas.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/allvisas",
+        element: <AllVisas />,
+      },
+      {
+        path: "/visa-details/:country",
+        element: <div>Visa details</div>,
+      },
+      {
+        path: "/authPage",
+        element: <LoginPage />,
+      },
+      {
+        path: "/profilePage",
+        element: (
+          <PrivateProvider>
+            <MyProfile />
+          </PrivateProvider>
+        ),
+      },
+      {
+        path: "/addVisa",
+        element: (
+          <PrivateProvider>
+            <AddVisa />
+          </PrivateProvider>
+        ),
+      },
+      {
+        path: "/myAddedVisa",
+        element: (
+          <PrivateProvider>
+            <MyAddedVisas />
+          </PrivateProvider>
+        ),
+      },
+      {
+        path: "/myVisaApplication",
+        element: (
+          <PrivateProvider>
+            <MyVisaApplications />
+          </PrivateProvider>
+        ),
+      },
+      {
+        path: "*",
+        element: <PageNotFound />,
+      },
+    ],
+  },
+]);
+
+export default router;
