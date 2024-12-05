8971c9c366aef2c1787376df67f7d74e51d5d2f1
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-12-05 23:56:09 +0600

    user icon component add

---
+++ src/components/common/UserIcon.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { FiLogOut } from "react-icons/fi";
+
+const UserIcon = ({ user }) => {
+  const firstName = user?.displayName?.split(" ")[0] || "Anonymous";
+  return (
+    <div className="flex-row flex gap-3 shadow-sm items-center justify-around">
+      <button className="btn btn-ghost ">
+        <div className="avatar online">
+          <div className="w-10 rounded-full">
+            <img
+              src={user?.photoURL || "default-avatar.png"}
+              alt={`${user?.displayName || "User"}'s avatar`}
+            />
+          </div>
+        </div>
+        <p>Logout</p>
+        {/* <FiLogOut className="w-10" /> */}
+      </button>
+    </div>
+  );
+};
+
+export default UserIcon;
