78656dccebee98ee1fec3c390a577efd3a6f7678
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-12-06 10:02:11 +0600

    loginPage components add

--- src/pages/AllVisas.jsx
+++ src/pages/AllVisas.jsx
@@ -6,7 +6,9 @@ const AllVisas = () => {
 
   useEffect(() => {
     const fetchVisas = async () => {
-      const response = await fetch("http://localhost:8000/visas");
+      const response = await fetch(
+        "https://backend-tau-vert-85.vercel.app/visas"
+      );
       const data = await response.json();
       setVisas(data);
     };
@@ -14,27 +16,20 @@ const AllVisas = () => {
   }, []);
 
   const handleSeeDetails = (visaId) => {
-    console.log("See details for visa:", visaId);
     // Add navigation or modal logic here
   };
 
   return (
-    <div className="flex flex-col items-center justify-center">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mx-auto">
-        {visas.map(
-          (visa) => (
-            console.log(visa),
-            (
-              <Card
-                key={visa._id}
-                visaCard={visa}
-                handleSeeDetails={handleSeeDetails}
-              />
-            )
-          )
-        )}
+    <div className="flex flex-col my-5 items-center justify-center">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mx-auto">
+        {visas.map((visa) => (
+          <Card
+            key={visa._id}
+            visaCard={visa}
+            handleSeeDetails={handleSeeDetails}
+          />
+        ))}
       </div>
-      )
     </div>
   );
 };
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import LoginForm from "../components/Auth/LoginForm";
+import { useLocation, useNavigate } from "react-router-dom";
+
+const LoginPage = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const from = location.state?.from?.pathname || "/"; // Default to "/" if from is undefined
+  
+  console.log(from);
+
+  return (
+    <div className="container mx-auto">
+      <LoginForm loginForm={from} />
+    </div>
+  );
+};
+
+export default LoginPage;
