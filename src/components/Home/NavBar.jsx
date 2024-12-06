3f3062d5f4fa71c4e3dafe31a00c6991e2e23752
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-12-06 10:52:12 +0600

    navbar section edit

--- src/components/Home/NavBar.jsx
+++ src/components/Home/NavBar.jsx
@@ -1,36 +1,75 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../providers/AuthProvider";
 import UserIcon from "../common/UserIcon";
+import { FaSun, FaMoon } from "react-icons/fa";
 
 const NavBar = () => {
-  const { user } = useContext(AuthContext);
+  const { user, signOutUser } = useContext(AuthContext);
+  const [theme, setTheme] = useState("dark");
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    const newTheme = theme === "dark" ? "light" : "dark";
+    setTheme(newTheme);
+  };
 
   const items = [
-    <li key="home" className="text-black font-roboto font-medium">
+    <li
+      key="home"
+      className={`font-roboto font-medium ${
+        theme === "dark" ? "text-white" : "text-black"
+      }`}>
       <Link to="/">Home</Link>
     </li>,
-    <li key="allvisas" className="text-black font-roboto font-medium">
+    <li
+      key="allvisas"
+      className={`font-roboto font-medium ${
+        theme === "dark" ? "text-white" : "text-black"
+      }`}>
       <Link to="/allvisas">All Visas</Link>
     </li>,
-    <li key="addVisa" className="text-black font-roboto font-medium">
+
+    <li
+      key="addVisa"
+      className={`font-roboto font-medium ${
+        theme === "dark" ? "text-white" : "text-black"
+      }`}>
       <Link to="/addVisa">Add Visa</Link>
     </li>,
-    <li key="myAddedVisa" className="text-black font-roboto font-medium">
+    <li
+      key="myAddedVisa"
+      className={`font-roboto font-medium ${
+        theme === "dark" ? "text-white" : "text-black"
+      }`}>
       <Link to="/myAddedVisa">My Added Visa</Link>
     </li>,
-    <li key="myVisaApplication" className="text-black font-roboto font-medium">
+    <li
+      key="myVisaApplication"
+      className={`font-roboto font-medium ${
+        theme === "dark" ? "text-white" : "text-black"
+      }`}>
       <Link to="/myVisaApplication">My Visa Application</Link>
     </li>,
-    <li key="profilePage" className="text-black font-roboto font-medium">
-      <Link to="/profilePage">Profile</Link>
-    </li>,
+    user && (
+      <li
+        key="profilePage"
+        className={`font-roboto font-medium ${
+          theme === "dark" ? "text-white" : "text-black"
+        }`}>
+        <Link to="/profilePage">Profile</Link>
+      </li>
+    ),
   ];
+
   return (
     <div>
       <div className="navbar bg-base-100 shadow p-3">
-        <div className="navbar-start">
-          <div className="dropdown">
+        <div className="block md:hidden ">
+          <div className="dropdown lg:hidden">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -52,16 +91,23 @@ const NavBar = () => {
               {items}
             </ul>
           </div>
-          <a className="btn btn-ghost text-xl">VisaEase</a>
+        </div>
+        <div className="navbar-start">
+          <Link to="/" className="btn btn-ghost text-xl">
+            VisaEase
+          </Link>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">{items}</ul>
         </div>
         <div className="navbar-end">
+          <button onClick={toggleTheme} className="btn btn-ghost rounded-full">
+            {theme === "dark" ? <FaSun /> : <FaMoon />}
+          </button>
           {user ? (
-            <Link to="/profilePage">
+            <button onClick={signOutUser} className="btn btn-ghost">
               <UserIcon user={user} />
-            </Link>
+            </button>
           ) : (
             <Link to="/authPage" className="btn">
               Login
